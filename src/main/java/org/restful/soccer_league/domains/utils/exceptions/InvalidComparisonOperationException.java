39c44b2370b5425a416a9af0e802d0ac9198ce43
JonasCodi <jonasgoms@hotmail.com>
2020-05-25 04:29:48 -0300

    Added new Exceptions;

---
+++ src/main/java/org/restful/soccer_league/domains/utils/exceptions/InvalidComparisonOperationException.java
@@ -0,0 +1,21 @@
+package org.restful.soccer_league.domains.utils.exceptions;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InvalidComparisonOperationException extends BusinessException {
+
+    @Getter
+    private String comparisonOperation;
+
+    @Getter
+    private String field;
+
+    public InvalidComparisonOperationException(String message, String resource,String field, String comparisonOperation) {
+        super(message, resource);
+        this.field = field;
+        this.comparisonOperation = comparisonOperation;
+    }
+}
