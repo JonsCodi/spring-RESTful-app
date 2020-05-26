4a62dda3a07a61e6c49f2a53929f4edac390ceed
JonasCodi <jonasgoms@hotmail.com>
2020-05-26 18:54:21 -0300

    Added Controller Pagination getAll();

--- src/main/java/org/restful/soccer_league/domains/team/api/v1/web/PersonController.java
+++ src/main/java/org/restful/soccer_league/domains/team/api/v1/web/PersonController.java
@@ -3,12 +3,23 @@ package org.restful.soccer_league.domains.team.api.v1.web;
 import com.github.fge.jsonpatch.JsonPatch;
 import com.github.fge.jsonpatch.mergepatch.JsonMergePatch;
 import lombok.RequiredArgsConstructor;
+import org.restful.soccer_league.domains.team.api.v1.web.assembler.PersonModelAssembler;
+import org.restful.soccer_league.domains.team.api.v1.web.model.PersonModel;
 import org.restful.soccer_league.domains.team.api.v1.web.request.BasePersonRequest;
 import org.restful.soccer_league.domains.team.entity.Person;
 import org.restful.soccer_league.domains.team.factory.PersonFactory;
 import org.restful.soccer_league.domains.team.service.IPersonService;
+import org.restful.soccer_league.domains.utils.api.web.v1.response.ResponseSuccessBody;
 import org.restful.soccer_league.domains.utils.components.PatchHelperComponent;
+import org.restful.soccer_league.domains.utils.components.ResponseEntityComponent;
 import org.restful.soccer_league.domains.utils.constants.PatchMediaType;
+import org.restful.soccer_league.domains.utils.enums.FieldsEnum;
+import org.restful.soccer_league.domains.utils.enums.FiltersEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,11 +29,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -31,6 +42,9 @@ public class PersonController {
 
     private final IPersonService<Person> personService;
     private final PatchHelperComponent patchHelperComponent;
+    private final PagedResourcesAssembler<Person> personPagedResourcesAssembler;
+    private final PersonModelAssembler personModelAssembler;
+    private final ResponseEntityComponent responseEntityComponent;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Person> create(@RequestBody BasePersonRequest basePersonRequest) {
@@ -45,8 +59,23 @@ public class PersonController {
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Person>> getAll() {
-        return ResponseEntity.ok(personService.findAll());
+    public ResponseEntity<ResponseSuccessBody> getAll(@RequestParam(value = "fields", required = false, defaultValue = "all") String fields,
+                                                      Pageable pageable) {
+        this.responseEntityComponent.setJsonFilters(new FiltersEnum[]{FiltersEnum.COACH, FiltersEnum.PLAYER});
+
+        Page<Person> persons = personService.findAll(pageable);
+        PagedModel<PersonModel> personModel = personPagedResourcesAssembler.toModel(persons, personModelAssembler);
+
+        if (personModel.getContent().isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        if (fields.equals(FieldsEnum.ALL.getField())) {
+            return responseEntityComponent.returnAllContent(personModel.getContent(), personModel.getLinks(), personModel.getMetadata());
+        }
+
+        return responseEntityComponent.returnPartialContent(fields,
+                personModel.getContent(), personModel.getLinks(), personModel.getMetadata());
     }
 
     @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
