e92f032fc553101de640cea3279430be21ff4408
JonasCodi <jonasgoms@hotmail.com>
2020-05-29 06:33:11 -0300

    Remove unnecessary import;

--- src/main/java/org/restful/soccer_league/domains/team/entity/Coach.java
+++ src/main/java/org/restful/soccer_league/domains/team/entity/Coach.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.Polymorphism;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
