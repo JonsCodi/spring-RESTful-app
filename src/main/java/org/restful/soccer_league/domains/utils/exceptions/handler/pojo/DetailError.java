b34e9175be5536f3818daeac2a94eb3cc6890dcd
JonasCodi <jonasgoms@hotmail.com>
2020-05-31 19:28:34 -0300

    Added NON_NULL to remove NULL values in JSON Attribute;

--- src/main/java/org/restful/soccer_league/domains/utils/exceptions/handler/pojo/DetailError.java
+++ src/main/java/org/restful/soccer_league/domains/utils/exceptions/handler/pojo/DetailError.java
@@ -1,10 +1,12 @@
 package org.restful.soccer_league.domains.utils.exceptions.handler.pojo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.io.Serializable;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 @AllArgsConstructor
 public class DetailError implements Serializable {
