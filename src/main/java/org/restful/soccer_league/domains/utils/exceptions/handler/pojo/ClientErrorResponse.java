abb5f7e0246bca5eeeaf3680c5aa106eafb1e229
JonasCodi <jonasgoms@hotmail.com>
2020-05-31 19:05:39 -0300

    Renamed and Added NON_NULL Json response;

--- src/main/java/org/restful/soccer_league/domains/utils/exceptions/handler/pojo/ClientErrorResponse.java
+++ src/main/java/org/restful/soccer_league/domains/utils/exceptions/handler/pojo/ClientErrorResponse.java
@@ -1,5 +1,6 @@
 package org.restful.soccer_league.domains.utils.exceptions.handler.pojo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import org.restful.soccer_league.domains.utils.enums.ClientResponseType;
 
@@ -7,8 +8,9 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
-public class ClientResponse implements Serializable {
+public class ClientErrorResponse implements Serializable {
 
     private long timestamp;
     private String date;
@@ -16,9 +18,9 @@ public class ClientResponse implements Serializable {
     private Object data;
     private Object details;
 
-    public ClientResponse(Object data, Object details) {
+    public ClientErrorResponse(Object data, Object details) {
         this.timestamp = System.currentTimeMillis();
-        this.date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss"));
+        this.date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         this.status = ClientResponseType.ERROR.getMessage();
         this.data = data;
         this.details = details;
