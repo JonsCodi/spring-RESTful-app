b596c62f7ef330ed4b8a897e083169a8f218f89b
JonasCodi <jonasgoms@hotmail.com>
2020-06-01 23:44:13 -0300

    Added Model Skeleton;

--- src/main/java/org/restful/soccer_league/domains/league/api/v1/web/model/SoccerLeagueModelAssembler.java
+++ src/main/java/org/restful/soccer_league/domains/league/api/v1/web/model/SoccerLeagueModelAssembler.java
@@ -1,9 +1,13 @@
 package org.restful.soccer_league.domains.league.api.v1.web.model;
 
+import org.apache.logging.log4j.util.Strings;
 import org.restful.soccer_league.domains.league.api.v1.web.SoccerLeagueController;
 import org.restful.soccer_league.domains.league.entity.SoccerLeague;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
 public class SoccerLeagueModelAssembler extends RepresentationModelAssemblerSupport<SoccerLeague, SoccerLeagueModel> {
@@ -14,6 +18,18 @@ public class SoccerLeagueModelAssembler extends RepresentationModelAssemblerSupp
 
     @Override
     public SoccerLeagueModel toModel(SoccerLeague entity) {
-        return null;
+        SoccerLeagueModel soccerLeagueModel = instantiateModel(entity);
+
+        soccerLeagueModel.add(linkTo(methodOn(SoccerLeagueController.class)
+                .get(Strings.EMPTY, entity.getId()))
+                .withSelfRel());
+
+        soccerLeagueModel.setId(entity.getId());
+        soccerLeagueModel.setName(entity.getName());
+        soccerLeagueModel.setCreatedAt(entity.getCreatedAt());
+        soccerLeagueModel.setUpdatedAt(entity.getUpdatedAt());
+        soccerLeagueModel.setDisabledAt(entity.getDisabledAt());
+
+        return soccerLeagueModel;
     }
 }
